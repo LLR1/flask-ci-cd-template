689ff19b5d42ccd51c8ae6a307df1a44e7b19c4e
LLR1 <alexmadov27@gmail.com>
2025-04-30 13:55:45 +0200

    Fixed routing & added favicon

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 
 
-app.route('/')
+@app.route('/')
 def home():
     return "Hello developer! This template kickstarts Flask + CI/CD."
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
+    
 if __name__ == '__main__':
     app.run(debug=True)
